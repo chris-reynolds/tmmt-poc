94e183f9d96f3e33d214747b3007a2acb2f47219
Chris Reynolds <chrisr@instantobjects.com>
2020-01-06 14:55:16 +1300

    testing original json model load

---
+++ src/bin/modelLoad.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+import 'dart:convert';
+
+
+void main() async {
+  var contents = File('jobuniverse.json').readAsStringSync();
+  var model = json.decode(contents);
+  print(model.toString());
+  var newModel = deepWrap(model);
+  print(newModel.toString());
+} // of main
+
+class MNode {
+  Map<dynamic,dynamic> _node;
+  MNode(this._node);
+} // of MNode
+
+dynamic deepWrap(dynamic origin) {
+  var result;
+ if (origin is Map) {
+    var newMap = (origin as Map).map((k1,v1)=>MapEntry(k1, deepWrap(v1)));
+    result = MNode(newMap);
+  }else   if (origin is Iterable) {
+   result = (origin as Iterable).map((n)=>deepWrap(n)).toList();
+ } else {
+    result = origin;
+  }
+  return result;
+}
