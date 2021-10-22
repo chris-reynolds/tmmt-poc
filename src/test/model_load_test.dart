76e071dd2b03a22c7ea7f6ae4fbe9435d3e80caa
Chris Reynolds <chrisr@instantobjects.com>
2021-10-23 11:43:42 +1300

    move test datafiles down into test/testdata

--- src/test/model_load_test.dart
+++ src/test/model_load_test.dart
@@ -5,21 +5,21 @@ import 'package:test/test.dart';
 
 void main() async {
   test('Load Job universe model and make sure it has 42 classes', () {
-    var juModel = (MModel('jobuniverse.json'));
+    var juModel = (MModel('test/testdata/jobuniverse.json'));
     var classes = juModel['classes'];
     assert(classes.length == 42);
   });
   test('Mustache is working', () {
-    var juModel = (MModel('jobuniverse.json'));
-    var writer = Writer('test.mustache', model: juModel);
+    var juModel = (MModel('test/testdata/jobuniverse.json'));
+    var writer = Writer('test/testdata/test.mustache', model: juModel);
     var output = writer.render();
     print(output);
     assert(output.split('\n').length == 45);
   });
   test('Platform CSS Parser is working', () {
-    var fred = TargetPlatform('test_platform.cds');
+    var fred = TargetPlatform('test/testdata/test_platform.cds');
     assert(fred.runtimeType.toString() == 'TargetPlatform');
-    var juModel = (MModel('jobuniverse.json'));
+    var juModel = (MModel('test/testdata/jobuniverse.json'));
     juModel.platform = fred;
   });
 } // of main
