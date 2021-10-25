d3ef6f894a4eca69af0e931eaf6ea11ffb8efc4d
Chris Reynolds <chrisr@instantobjects.com>
2021-10-25 17:23:45 +1300

    Move test dir to a variable

--- src/test/model_load_test.dart
+++ src/test/model_load_test.dart
@@ -3,23 +3,50 @@ import '../lib/writer.dart';
 import '../lib/target_platform.dart';
 import 'package:test/test.dart';
 
+var TEST_DIR = 'test/testdata';
 void main() async {
   test('Load Job universe model and make sure it has 42 classes', () {
-    var juModel = (MModel('test/testdata/jobuniverse.json'));
+    var juModel = (MModel('$TEST_DIR/jobuniverse.json'));
     var classes = juModel['classes'];
     assert(classes.length == 42);
   });
   test('Mustache is working', () {
-    var juModel = (MModel('test/testdata/jobuniverse.json'));
-    var writer = Writer('test/testdata/test.mustache', model: juModel);
+    var juModel = (MModel('$TEST_DIR/jobuniverse.json'));
+    var writer = Writer('$TEST_DIR/test.mustache', model: juModel);
     var output = writer.render();
     print(output);
     assert(output.split('\n').length == 45);
   });
   test('Platform CSS Parser is working', () {
-    var fred = TargetPlatform('test/testdata/test_platform.cds');
+    var fred = TargetPlatform('$TEST_DIR/test_platform.cds');
     assert(fred.runtimeType.toString() == 'TargetPlatform');
-    var juModel = (MModel('test/testdata/jobuniverse.json'));
+    var juModel = (MModel('$TEST_DIR/jobuniverse.json'));
     juModel.platform = fred;
   });
 } // of main
+
+
+// void main_test() {
+//   var source = '''
+// 	  {{# names }}
+//             <div>{{ lastname }}, {{ firstname }}z	  {{/ names }}
+// 	  {{^ names }}
+// 	    <div>No names.</div>
+// 	  {{/ names }}
+//     {{ address.line1 }}
+// 	  {{! I am a comment. }}
+// 	''';
+
+//   var template = Template(source, name: 'template-filename.html');
+
+//   var output = template.renderString({
+//     'names': [
+//       {'firstname': 'Greg', 'lastname': 'Lowe'},
+//       {'firstname': 'chris', 'lastname': 'reynolds'},
+//       {'firstname': 'Bob', 'lastname': 'Johnson'}
+//     ],
+//     'address': {'line1': 'Omokoroa Rd'}
+//   });
+
+//   print(output);
+// }
