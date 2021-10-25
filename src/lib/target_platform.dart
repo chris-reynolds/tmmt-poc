15640d0d8234e21e68686ec954095e955302c8c8
Chris Reynolds <chrisr@instantobjects.com>
2021-10-25 17:25:09 +1300

    Add modelfacade reference to platform to see if it adds face to coverage stats

--- src/lib/target_platform.dart
+++ src/lib/target_platform.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
 import 'package:csslib/parser.dart' as css;
 import 'package:csslib/visitor.dart';
+import 'modelFacade.dart';
+
+var fred = RuleBlock();
 
 class TargetPlatform {
   late final StyleSheet _sheet;
@@ -9,6 +12,11 @@ class TargetPlatform {
     print(_sheet);
     var fred = _sheet.topLevels;
     print(fred.length);
+    RuleSet bill = fred[3] as RuleSet;
+    var sel = bill.selectorGroup?.selectors[0];
+    var dec = bill.declarationGroup.declarations[0];
+    print(sel);
+    print(dec);
   }
   String evaluate(node, key) {
     return 'qqqqqq $key from platform';
